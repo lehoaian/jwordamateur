08115e86243de3beda1dd3f727d9dc6c15209685
manh1312 <manh1312@e4bc820f-f74e-0410-870d-f30df236eb41>
2008-09-30 17:53:43 +0000

    Them Kanji Project

---
+++ Kanji/Kanji/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Kanji
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblKanji = new System.Windows.Forms.Label();
+            this.lblKunyomi = new System.Windows.Forms.Label();
+            this.lblOnyomi = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(1, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "b";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(112, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblKanji
+            // 
+            this.lblKanji.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblKanji.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKanji.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblKanji.Location = new System.Drawing.Point(0, 0);
+            this.lblKanji.Name = "lblKanji";
+            this.lblKanji.Size = new System.Drawing.Size(199, 37);
+            this.lblKanji.TabIndex = 2;
+            this.lblKanji.Text = "a";
+            this.lblKanji.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblKunyomi
+            // 
+            this.lblKunyomi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKunyomi.ForeColor = System.Drawing.Color.Indigo;
+            this.lblKunyomi.Location = new System.Drawing.Point(12, 76);
+            this.lblKunyomi.Name = "lblKunyomi";
+            this.lblKunyomi.Size = new System.Drawing.Size(93, 45);
+            this.lblKunyomi.TabIndex = 3;
+            this.lblKunyomi.Text = "c";
+            // 
+            // lblOnyomi
+            // 
+            this.lblOnyomi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOnyomi.ForeColor = System.Drawing.Color.Olive;
+            this.lblOnyomi.Location = new System.Drawing.Point(111, 76);
+            this.lblOnyomi.Name = "lblOnyomi";
+            this.lblOnyomi.Size = new System.Drawing.Size(76, 45);
+            this.lblOnyomi.TabIndex = 3;
+            this.lblOnyomi.Text = "d";
+            this.lblOnyomi.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(15, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 160);
+            this.Controls.Add(this.lblOnyomi);
+            this.Controls.Add(this.lblKunyomi);
+            this.Controls.Add(this.lblKanji);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kanji";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblKanji;
+        private System.Windows.Forms.Label lblKunyomi;
+        private System.Windows.Forms.Label lblOnyomi;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Kanji/Kanji/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Kanji
+{
+    public partial class Form1 : Form
+    {
+        List<Word> list = new List<Word>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(new FileStream("data.txt", FileMode.Open, FileAccess.Read));
+            string temp = null;
+            while ((temp = sr.ReadLine()) != null)
+            {
+                Word w = new Word(temp);
+                list.Add(w);
+            }
+
+            button1.PerformClick();
+        }
+        Random rd = new Random();
+        Word current = new Word();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.lblKanji.Text = "";
+            this.lblKunyomi.Text = "";
+            this.lblOnyomi.Text = "";
+
+            if (list.Count == 0)
+                return;
+
+            if (IsAllWordShown())
+            {
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    list[i].IsShown = false;
+                }
+            }
+
+            int index = rd.Next(0, list.Count);
+            while (list[index].IsShown)
+            {
+                index = rd.Next(0, list.Count);
+            }
+            this.label1.Text = list[index].Name;
+            this.current = list[index];
+            list[index].IsShown = true;
+        }
+
+        private bool IsAllWordShown()
+        {
+            foreach (Word w in list)
+            {
+                if (!w.IsShown)
+                    return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.lblKanji.Text = current.Kanji;
+            this.lblOnyomi.Text = current.Onyomi;
+            this.lblKunyomi.Text = current.Kunyomi;
+        }
+    }
+
+    public class Word
+    {
+        public string Name;
+        public string Kanji;
+        public string Kunyomi;
+        public string Onyomi;
+        public bool IsShown = false;
+
+        public Word(string strWord)
+        {
+            string[] strs = strWord.Split(new char[] { ',' });
+            if (strs.Length == 4)
+            {
+                Name = strs[0];
+                Kanji = strs[1];
+                Kunyomi = strs[2];
+                Onyomi = strs[3];
+                IsShown = false;
+            }
+        }
+
+        public Word()
+        {
+        }        
+    }
+}
